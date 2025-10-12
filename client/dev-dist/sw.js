b4b63210f351f604ca35c3605f95b38d223ba394
Tim Quattrochi <timq82@gmail.com>
2025-10-12 05:47:31 -0400

    fix: update revision for index.html in service worker

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-af2f661e'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ev2e5jqc6e8"
+    "revision": "0.csbu0kc46l"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
