ed013638606823801485a843ec5c9efc555a7476
Tim Quattrochi <timq82@gmail.com>
2025-10-15 00:13:20 -0400

    fix(sw): Update revision for index.html in service worker cache

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p4g1hdjd738"
+    "revision": "0.gd8ffo6d2c8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
